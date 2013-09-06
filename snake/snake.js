b641b009357d8ce00e20eb1702027a4a6326b769
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-06 21:59:24 +0200

    Snake is now moving forward

--- snake/snake.js
+++ snake/snake.js
@@ -5,10 +5,14 @@
 
   JS_SNAKE.game = (function () {
     var ctx;
+    var snake;
+
     var frameInterval = 50;
-    var size = {
+
+    JS_SNAKE.size = {
       width : 500,
-      height: 500
+      height: 500,
+      block : 10
     };
     var coord = {
       x : 0,
@@ -17,26 +21,61 @@
 
     function init() {
       var $canvas = $('#js-snake');
-      $canvas.attr('width', size.width);
-      $canvas.attr('height', size.height);
+      $canvas.attr('width', JS_SNAKE.size.width);
+      $canvas.attr('height', JS_SNAKE.size.height);
+
       ctx = $canvas[0].getContext('2d');
+      snake = JS_SNAKE.snake();
 
       loop();
-    };
+    }
 
     function loop() {
-      coord.x += 2;
-      coord.y += 4;
-      ctx.clearRect(0, 0, 500, 500);
-      ctx.fillStyle = '#fe57a1';
-      ctx.fillRect(coord.x, coord.y, 30, 50);
+      ctx.clearRect(0, 0, JS_SNAKE.size.width,JS_SNAKE.size.height);
+      snake.advance();
+      snake.draw(ctx);
       setTimeout(loop, frameInterval);
     }
 
     return {
-      init : init
-    }
+      init: init
+    };
   })();
+
+  JS_SNAKE.snake = function (){
+    var position = [];
+    position.push([6, 4]);
+    position.push([5, 4]);
+    position.push([4, 4]);
+    var direction = 'right';
+
+    function drawBlock(ctx, pos) {
+      var x = JS_SNAKE.size.block * pos[0];
+      var y = JS_SNAKE.size.block * pos[1];
+      ctx.fillRect(x, y, JS_SNAKE.size.block, JS_SNAKE.size.block);
+    }
+
+    function draw(ctx) {
+      ctx.save();
+      ctx.fillStyle = 'tomato';
+      for (var i = 0; i < position.length; i++) {
+        drawBlock(ctx, position[i]);
+      }
+      ctx.restore();
+    }
+
+    function advance() {
+      var nextPosition = position[0].slice();
+      nextPosition[0]++;
+      position.unshift(nextPosition);
+      position.pop();
+    }
+
+    return {
+      draw: draw,
+      advance: advance
+    }
+  };
 
   $(document).ready(function() {
     JS_SNAKE.game.init();
