6bacf4282f96b6d21343ca6172b25858bce33f95
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-08 14:40:37 +0200

    Add movement to the stage

--- pixi/main.js
+++ pixi/main.js
@@ -7,21 +7,31 @@
 
   window.requestAnimationFrame(animate);
 
-  var bunny = PIXI.Sprite.fromImage('bunny.png');
+  var texture = PIXI.Texture.fromImage('bunny.png');
+  var bunny = new PIXI.Sprite(texture);
 
   bunny.anchor.x = 0.5;
   bunny.anchor.y = 0.5;
-  bunny.position.x = 200;
-  bunny.position.y = 150;
+  bunny.position.x = 50;
+  bunny.position.y = 50;
 
   stage.addChild(bunny);
 
   function animate() {
     window.requestAnimationFrame(animate);
 
-    bunny.rotation += 0.05;
+    bunny.rotation -= 0.05;
+
+    bunny.position.x += backAndForth(0, bunny.position.x, 400);
 
     renderer.render(stage);
   }
 
 })(window.PIXI);
+
+function backAndForth(lowest, current, highest) {
+  var threshold = 25;
+  this.forth = (typeof this.forth === 'undefined') ? true : this.forth;
+  this.forth = this.forth && current < highest - threshold || current < lowest + threshold;
+  return this.forth ? 1 : -1;
+}
