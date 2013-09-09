bd2990f1aa750f1e1f5cbe441efe482a8f16a135
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-09 16:16:09 +0200

    Detect when a trail is out of the letter

--- letterpaint/main.js
+++ letterpaint/main.js
@@ -11,7 +11,13 @@
   cx.lineCap = 'round';
   cx.strokeStyle = 'rgb(30, 30, 60)';
   cx.font = 'bold 350px helvetica';
-  cx.fillStyle = 'rgb(255, 0, 0)';
+  var letterColor = {
+    r: 200,
+    g: 30,
+    b: 30,
+    a: 1
+  };
+  cx.fillStyle = 'rgba(' + letterColor.r + ', ' + letterColor.g + ', ' + letterColor.b + ', ' + letterColor.a + ')';
   cx.textBaseline = 'middle';
   var center = {
     x: (c.width - cx.measureText(letter).width) / 2,
@@ -24,47 +30,73 @@
     y: null
   };
 
+  var pixels = cx.getImageData(0, 0, c.width, c.height);
+
+  function getPointColor(x, y) {
+    // 4 cells per point, all in a one-dimension array.
+    var i = (x + y * pixels.width) * 4;
+    return {
+      r: pixels.data[i],
+      g: pixels.data[i + 1],
+      b: pixels.data[i + 2],
+      a: pixels.data[i + 3]
+    };
+  }
+
+  function isOnLetter(x, y) {
+    var color = getPointColor(x, y);
+    return color.a !== 0;
+  }
+
   function isOnCanvas(p) {
     return 0 < p.x && p.x <= c.width && 0 < p.y && p.y <= c.height;
   }
 
   function paint(x, y) {
-    cx.beginPath();
-    if (isOnCanvas(lastPosition)) {
-      cx.moveTo(lastPosition.x, lastPosition.y);
+    if (isOnLetter(x, y)) {
+      cx.beginPath();
+      if (isOnCanvas(lastPosition)) {
+        cx.moveTo(lastPosition.x, lastPosition.y);
+      }
+      cx.lineTo(x, y);
+      cx.stroke();
+      cx.closePath();
     }
-    cx.lineTo(x, y);
-    cx.stroke();
-    cx.closePath();
-  }
-
-  c.addEventListener('mouseup', function (e) {
+  else {
     mouseDown = false;
-    e.preventDefault();
-  });
-  c.addEventListener('mousedown', function (e) {
-    mouseDown = true;
-    e.preventDefault();
-  });
-  c.addEventListener('mousemove', function (e) {
-    if (mouseDown) {
-      paint(e.clientX, e.clientY);
-    }
-    lastPosition.x = e.clientX;
-    lastPosition.y = e.clientY;
-  });
+    alert(':(');
+  }
+}
+
+c.addEventListener('mouseup', function (e) {
+  mouseDown = false;
+  e.preventDefault();
+});
+c.addEventListener('mousedown', function (e) {
+  mouseDown = true;
+  e.preventDefault();
+});
+c.addEventListener('mousemove', function (e) {
+  if (mouseDown) {
+    paint(e.clientX, e.clientY);
+  }
+  lastPosition.x = e.clientX;
+  lastPosition.y = e.clientY;
+});
 
 // Prevents long trails when the mouse wanders outside of canvas.
-  document.addEventListener('mousemove', function (e) {
-    var position = {
-      x: e.clientX,
-      y: e.clientY
-    };
-    if (!isOnCanvas(position)) {
-      lastPosition.x = null;
-      lastPosition.y = null;
-    }
-  })
+document.addEventListener('mousemove', function (e) {
+  var position = {
+    x: e.clientX,
+    y: e.clientY
+  };
+  if (!isOnCanvas(position)) {
+    lastPosition.x = null;
+    lastPosition.y = null;
+  }
+});
 })();
+
+
 
 
