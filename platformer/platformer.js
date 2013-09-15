3401d1626230fae0e86da7ec6a960afcdd245244
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-15 21:51:31 +0200

    Rendering the map data from level.json.

--- platformer/platformer.js
+++ platformer/platformer.js
@@ -7,13 +7,13 @@
   }
 
   /**
-   * Variables.
+   * Parameters.
    */
 
   var map = {
     tile       : 32,
     tileWidth  : 64,
-    tileHeight : 48,
+    tileHeight : 48
   };
 
   var canvas = document.getElementById('js-platformer');
@@ -21,6 +21,27 @@
 
   canvas.width  = map.tileWidth * map.tile;
   canvas.height = map.tileHeight * map.tile;
+
+  var palette = {
+    black:  '#000',
+    yellow: '#ECD078',
+    brick:  '#D95B43',
+    pink:   '#C02942',
+    purple: '#542437',
+    grey:   '#333',
+    slate:  '#53777A',
+    gold:   'gold',
+    steel:  'steelblue',
+    cyan:   'cyan',
+    dark:   'darkcyan'
+  };
+  var colors = [
+    palette.yellow,
+    palette.brick,
+    palette.pink,
+    palette.purple,
+    palette.grey
+  ];
 
   var time = {
     counter : 0,
@@ -32,6 +53,17 @@
   var fps      = 60;
   var step     = 1 / fps;
   var counter  = 0;
+
+  var tiles = [];
+
+  /**
+   * Converter functions.
+   */
+
+  // Returns the cell data for a given point.
+  function getTileData(tx, ty) {
+   return tiles[tx + (ty * map.tileWidth)];
+  }
 
   /**
    * Setup functions.
@@ -49,7 +81,28 @@
   }
 
   function setup(map) {
-    console.dir(map);
+    tiles = map.layers[0].data;
+  }
+
+  /**
+   * Update functions.
+   */
+
+  /**
+   * Render functions.
+   */
+
+  function renderMap(ctx) {
+    var tileData = null;
+    for (var y = 0; y < map.tileHeight; y++) {
+      for (var x = 0; x < map.tileWidth; x++) {
+        tileData = getTileData(x, y);
+        if (tileData) {
+          ctx.fillStyle = colors[tileData - 1];
+          ctx.fillRect(x * map.tile, y * map.tile, map.tile, map.tile);
+        }
+      }
+    }
   }
 
   /**
@@ -61,6 +114,7 @@
 
   function render(ctx, frame, dt) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    renderMap(ctx);
   }
 
   function frame() {
