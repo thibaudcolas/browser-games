eb9377b77a6d1a4a2f592b41569e7706d1858254
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-27 21:27:09 +0200

    Transfer JS initialization code in HTML

--- js-hero/js/Hero.js
+++ js-hero/js/Hero.js
@@ -1,3 +1,22 @@
 var Hero = Backbone.View.extend({
+  initialize: function () {
+    this.$body = $('body');
+    this.build();
+  },
 
+  build: function () {
+    if (this.browserSupportsTouch()) {
+      console.log('supports touch');
+    }
+  },
+
+  browserSupportsTouch: function () {
+    try {
+      document.createEvent("TouchEvent");
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
+  }
 });
