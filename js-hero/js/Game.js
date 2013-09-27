d3c76762338e3f9bdf235accfe1033c778261337
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-27 21:37:34 +0200

    render horizon

--- js-hero/js/Game.js
+++ js-hero/js/Game.js
@@ -1,9 +1,15 @@
 var Game = Backbone.View.extend({
+  template:
+    '<div class="scene">' +
+      '<div class="horizon"></div>' +
+    '</div>',
+
   initialize: function () {
     this.data = [];
     this.$document = $(document);
     this.$window = $(window);
     this.$body = $('body');
+    this.$el.html(this.template);
     // this.onInterval = this.onInterval.bind(this);
   }
 });
