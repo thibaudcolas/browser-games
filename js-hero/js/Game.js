11f5bbe101f1fec5ae4f9ce64bc2ddaae21610f6
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-27 21:47:51 +0200

    Register keys

--- js-hero/js/Game.js
+++ js-hero/js/Game.js
@@ -4,6 +4,14 @@ var Game = Backbone.View.extend({
       '<div class="horizon"></div>' +
     '</div>',
 
+  options: {
+    escKey : 27
+  },
+
+  events: {
+
+  },
+
   initialize: function () {
     this.data = [];
     this.$document = $(document);
@@ -11,5 +19,35 @@ var Game = Backbone.View.extend({
     this.$body = $('body');
     this.$el.html(this.template);
     // this.onInterval = this.onInterval.bind(this);
+    this.attach();
+  },
+
+  setup: function () {
+
+  },
+
+  attach: function () {
+    this.$document.on('keydown', this.onKeydown.bind(this));
+    this.$window.on('resize', this.onWindowResize.bind(this));
+    this.$window.on('scroll touchmove', function (evt) { evt.preventDefault(); });
+    this.$body.on('scroll touchmove', function (evt) { evt.preventDefault(); });
+  },
+
+  onWindowResize: function () {
+
+  },
+
+  onKeydown: function (evt) {
+    if (evt.altKey || evt.ctrlKey || evt.metaKey) {
+      return;
+    }
+    if (evt.which === this.options.escKey) {
+      evt.preventDefault();
+    }
+    console.log(evt.which);
+  },
+
+  render: function () {
+
   }
 });
