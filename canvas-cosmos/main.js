bef51a9a9168f739be93cd3725761a6288dd6bc2
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-29 23:30:43 +0200

    First moving cosmos !

--- canvas-cosmos/main.js
+++ canvas-cosmos/main.js
@@ -1,4 +1,6 @@
 (function () {
+  'use strict';
+
   var canvas = document.getElementById('canvas');
   var canvas2 = document.getElementById('canvas2');
   var canvas3 = document.getElementById('canvas3');
@@ -11,7 +13,36 @@
   img.src = 'cosmos.jpg';
   ctx3.drawImage(img, 0, 0, 570, 570);
 
+  var puffs = [];
+
+  var Puff = function (p) {
+    this.p = p;
+    this.opacity = Math.sin(p * 0.05) * 0.5;
+    this.sx = (Math.random() * 285) >> 0;
+    this.sy = (Math.random() * 285) >> 0;
+
+    this.move = function (time) {
+      p = this.p + 0.3 * time;
+      this.opacity = Math.sin(p * 0.05) * 0.5;
+      if (this.opacity < 0) {
+        p = this.opacity = 0;
+        this.sx = (Math.random() * 285) >> 0;
+        this.sy = (Math.random() * 285) >> 0;
+      }
+      this.p = p;
+      ctx.globalAlpha = this.opacity;
+      ctx.drawImage(canvas3, this.sx + p, this.sy + p, 285 - (p * 2),285 - (p * 2), 0,0, w, h);
+    };
+  };
+
+  puffs.push(new Puff(0));
+  puffs.push(new Puff(20));
+  puffs.push(new Puff(40));
+
   function loop () {
+    puffs[0].move(2);
+
+    ctx2.drawImage(canvas,0 ,0 ,570 ,570);
     window.requestAnimationFrame(loop, canvas);
   }
 
