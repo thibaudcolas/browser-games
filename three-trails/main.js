3f1ae8e5410ec1c549bb680da75382f97dc5bc14
Thibaud Colas <thibaudcolas@gmail.com>
2013-10-05 17:51:44 +0200

    Setting up the animation calculations.

--- three-trails/main.js
+++ three-trails/main.js
@@ -1,4 +1,28 @@
 (function (THREE) {
   'use strict';
 
+  var view = {
+    w: window.innerWidth,
+    h: window.innerHeight,
+    center : {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2
+    }
+  };
+
+  var mouse = {
+    x: 0,
+    y: 0
+  };
+
+  init();
+
+  function init() {
+
+  }
+
+  function loop() {
+    window.requestAnimationFrame(loop);
+  }
+
 })(window.THREE);
