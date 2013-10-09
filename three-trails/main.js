9dece7002068da5f8b6d90d868dca3d636ab93a3
Thibaud Colas <thibaudcolas@gmail.com>
2013-10-09 08:27:35 +0200

    Set up rendering engine

--- three-trails/main.js
+++ three-trails/main.js
@@ -15,13 +15,26 @@
     y: 0
   };
 
+  var camera;
+  var scene;
+  var renderer;
+
   init();
+  loop();
 
   function init() {
+    camera = new THREE.PerspectiveCamera(75, view.w / view.h, 0.1, 1000);
+    scene = new THREE.Scene();
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(view.w, view.h);
 
+    document.body.appendChild(renderer.domElement);
   }
 
   function loop() {
+    camera.updateMatrix();
+    renderer.render(scene, camera);
+
     window.requestAnimationFrame(loop);
   }
 
