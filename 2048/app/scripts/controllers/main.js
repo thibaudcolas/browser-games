086340c0f6feb6c95a032ff613e625147960cdc8
Thibaud Colas <thibaudcolas@gmail.com>
2014-04-14 02:31:54 +0200

    2048: start experiment

---
+++ 2048/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('twentyfourtyeightApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
