b890f933e5a52ad2db4af371612e9e54431ddda4
Thibaud Colas <thibaudcolas@gmail.com>
2015-03-29 18:36:31 +1300

    Render Pacman arena

--- reactive-pacman/pacman.js
+++ reactive-pacman/pacman.js
@@ -30,6 +30,20 @@ window.Pacman = (function() {
       }
     };
 
+    this.TileCode = {
+      WALL: '#',
+      POINT: '.',
+      BIGP: 'o',
+      EMPTY: ' '
+    };
+
+    this.TileDisplay = {
+      WALL: new ut.Tile(' ', 0, 0, 0, 100, 100, 100),
+      POINT: new ut.Tile('▪', 150, 0, 150),
+      BIGP: new ut.Tile('O', 150, 0, 150),
+      EMPTY: new ut.Tile(' ', 100, 100, 100)
+    };
+
     this.term = new ut.Viewport(this.container, 50, 30);
 
     // ut.initInput(this.onKeyDown.bind(this));
@@ -64,13 +78,38 @@ window.Pacman = (function() {
     ];
   };
 
+  Pacman.prototype.getPacmanTile = function(x, y) {
+    var tileData = this.map[y][x];
+    var tileDisplay;
+
+    switch (tileData) {
+      case this.TileCode.POINT:
+        tileDisplay = this.TileDisplay.POINT;
+        break;
+      case this.TileCode.BIGP:
+        tileDisplay = this.TileDisplay.BIGP;
+        break;
+      case this.TileCode.WALL:
+        tileDisplay = this.TileDisplay.WALL;
+        break;
+      case this.TileCode.EMPTY:
+        tileDisplay = this.TileDisplay.EMPTY;
+        break;
+      default:
+        tileDisplay = ut.NULLTILE;
+        break;
+    }
+
+    return tileDisplay;
+  };
+
   Pacman.prototype.putTiles = function() {
     var base = this.termConfig.gameArea;
     var rowLength = this.map[0].length;
 
     for (var y = 0; y < this.map.length; y++) {
       for (var x = 0; x < rowLength; x++) {
-        this.term.put(new ut.Tile('X', 150, 75, 0), base.x + x, base.y + y);
+        this.term.put(this.getPacmanTile(x, y), base.x + x, base.y + y);
       }
     }
   };
