beb206a9304dc822138bf908c7149aaf581adb7d
Thibaud Colas <thibaudcolas@gmail.com>
2015-03-29 19:47:52 +1300

    Add a moving Pacman

--- reactive-pacman/main.js
+++ reactive-pacman/main.js
@@ -8,4 +8,26 @@ function restartGame(container) {
   var game = new Pacman(container);
 
   game.start();
+
+  var moveStream = Bacon.fromBinder(function(sink) {
+    game.onPacmanMove = function(moveV) {
+      sink(moveV);
+    };
+  });
+
+  moveStream.onValue(function(moveV) {
+    if (!moveV) {
+      restartGame(container);
+      return;
+    }
+
+    game.movePacman(moveV);
+  });
+
+  var combinedTickStream = new Bacon.Bus();
+
+  combinedTickStream.plug(moveStream);
+  combinedTickStream.subscribe(function() {
+    game.tick();
+  });
 }
--- reactive-pacman/pacman.js
+++ reactive-pacman/pacman.js
@@ -1,9 +1,31 @@
-'use strict';
-
 window.Pacman = (function() {
+  'use strict';
+
   var Pacman = function(container) {
     this.gameOver = true;
     this.container = container;
+    this.onPacmanMove = null;
+
+    this.PacmanTile = {
+      LEFT: [new ut.Tile('ᗤ', 255, 248, 74), new ut.Tile('ᗞ', 255, 248, 74)],
+      RIGHT: [new ut.Tile('ᗧ', 255, 248, 74), new ut.Tile('ᗡ', 255, 248, 74)],
+      UP: [new ut.Tile('ᗢ', 255, 248, 74), new ut.Tile('ᗜ', 255, 248, 74)],
+      DOWN: [new ut.Tile('ᗣ', 255, 248, 74), new ut.Tile('ᗝ', 255, 248, 74)]
+    };
+
+    this.MapTileCode = {
+      WALL: '#',
+      POINT: '.',
+      BIGP: 'o',
+      EMPTY: ' '
+    };
+
+    this.MapTileDisplay = {
+      WALL: new ut.Tile(' ', 0, 0, 0, 0, 71, 241),
+      POINT: new ut.Tile('・', 238, 179, 149),
+      BIGP: new ut.Tile('●', 238, 179, 149),
+      EMPTY: new ut.Tile(' ', 100, 100, 100)
+    };
   }
 
   Pacman.prototype.start = function() {
@@ -12,7 +34,7 @@ window.Pacman = (function() {
     this.p1 = {
       x: 14,
       y: 12,
-      tile: '€' //this.TilePacmanLeft[0]
+      tile: this.PacmanTile.LEFT[0]
     };
 
     this.ghosts = [];
@@ -30,24 +52,9 @@ window.Pacman = (function() {
       }
     };
 
-    this.TileCode = {
-      WALL: '#',
-      POINT: '.',
-      BIGP: 'o',
-      EMPTY: ' '
-    };
-
-    this.TileDisplay = {
-      WALL: new ut.Tile(' ', 0, 0, 0, 0, 71, 241),
-      POINT: new ut.Tile('・', 238, 179, 149),
-      BIGP: new ut.Tile('●', 238, 179, 149),
-      EMPTY: new ut.Tile(' ', 100, 100, 100)
-    };
-
     this.term = new ut.Viewport(this.container, 50, 30, 'auto', true);
 
-    // ut.initInput(this.onKeyDown.bind(this));
-
+    ut.initInput(this.onKeyDown.bind(this));
     this.tick();
   };
 
@@ -78,29 +85,29 @@ window.Pacman = (function() {
     ];
   };
 
-  Pacman.prototype.getPacmanTile = function(x, y) {
+  Pacman.prototype.getMapTile = function(x, y) {
     var tileData = this.map[y][x];
-    var tileDisplay;
+    var mapTileDisplay;
 
     switch (tileData) {
-      case this.TileCode.POINT:
-        tileDisplay = this.TileDisplay.POINT;
+      case this.MapTileCode.POINT:
+        mapTileDisplay = this.MapTileDisplay.POINT;
         break;
-      case this.TileCode.BIGP:
-        tileDisplay = this.TileDisplay.BIGP;
+      case this.MapTileCode.BIGP:
+        mapTileDisplay = this.MapTileDisplay.BIGP;
         break;
-      case this.TileCode.WALL:
-        tileDisplay = this.TileDisplay.WALL;
+      case this.MapTileCode.WALL:
+        mapTileDisplay = this.MapTileDisplay.WALL;
         break;
-      case this.TileCode.EMPTY:
-        tileDisplay = this.TileDisplay.EMPTY;
+      case this.MapTileCode.EMPTY:
+        mapTileDisplay = this.MapTileDisplay.EMPTY;
         break;
       default:
-        tileDisplay = ut.NULLTILE;
+        mapTileDisplay = ut.NULLTILE;
         break;
     }
 
-    return tileDisplay;
+    return mapTileDisplay;
   };
 
   Pacman.prototype.putTiles = function() {
@@ -109,14 +116,91 @@ window.Pacman = (function() {
 
     for (var y = 0; y < this.map.length; y++) {
       for (var x = 0; x < rowLength; x++) {
-        this.term.put(this.getPacmanTile(x, y), base.x + x, base.y + y);
+        this.term.put(this.getMapTile(x, y), base.x + x, base.y + y);
       }
     }
+  };
+
+  Pacman.prototype.putPlayer = function() {
+    var base = this.termConfig.gameArea;
+
+    this.term.put(this.p1.tile, base.x + this.p1.x, base.y + this.p1.y);
+  };
+
+  Pacman.prototype.onKeyDown = function(key) {
+    var moveV = {
+      x: 0,
+      y: 0
+    };
+
+    switch (key) {
+      case ut.KEY_UP:
+        moveV.y--;
+        break;
+      case ut.KEY_LEFT:
+        moveV.x--;
+        break;
+      case ut.KEY_DOWN:
+        moveV.y++;
+        break;
+      case ut.KEY_RIGHT:
+        moveV.x++;
+        break;
+      case ut.KEY_SPACE:
+        if (this.gameOver) {
+          moveV = null;
+        }
+        break;
+      default:
+        break;
+    }
+
+    if (typeof this.onPacmanMove === 'function') {
+      this.onPacmanMove(moveV);
+    }
+  };
+
+  Pacman.prototype.canMoveTo = function(x, y) {
+    var tile = this.getMapTile(x, y);
+    return [this.MapTileDisplay.POINT, this.MapTileDisplay.BIGP, this.MapTileDisplay.EMPTY].indexOf(tile) !== -1;
+  };
+
+  Pacman.prototype.movePacman = function(p1V) {
+    var p1 = {
+      x: this.p1.x + p1V.x,
+      y: this.p1.y + p1V.y,
+      tile: this.PacmanTile.DOWN
+    };
+
+    if (this.gameOver) {
+      return;
+    }
+
+    if (!this.canMoveTo(p1.x, p1.y)) {
+      return;
+    }
+
+    if (p1V.x < 0) {
+      p1.tile = this.PacmanTile.LEFT;
+    } else if (p1V.x > 0) {
+      p1.tile = this.PacmanTile.RIGHT;
+    } else if (p1V.y < 0) {
+      p1.tile = this.PacmanTile.UP;
+    }
+
+    if (p1.tile[0] === this.p1.tile) {
+      p1.tile = p1.tile[1];
+    } else {
+      p1.tile = p1.tile[0];
+    }
+
+    this.p1 = p1;
   };
 
   Pacman.prototype.tick = function() {
     this.term.clear();
     this.putTiles();
+    this.putPlayer();
     this.term.render();
   };
 
